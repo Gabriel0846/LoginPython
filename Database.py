57e8a121457d204c9c7c3d754809c9db030e9e78
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-04 10:29:05 -0300

    codado uma parte do DataBase

---
+++ Database.py
@@ -0,0 +1,24 @@
+import datetime
+
+class DataBase:
+    def __init__(self, filename):
+        self.filename = filename
+        self.users = None
+        self.file = None
+        self.load()
+
+    def load(self):
+        self.file = open(self.filename, "r")
+        self.users = {}
+
+        for line in self.file:
+            email, password, name, created = line.strip().split(";")
+            self.users[email] = (password, name, created)
+
+        self.file.close()
+
+    def get_user(self, email):
+        if email in self.users:
+            return self.users[email]
+        else:
+            return -1
